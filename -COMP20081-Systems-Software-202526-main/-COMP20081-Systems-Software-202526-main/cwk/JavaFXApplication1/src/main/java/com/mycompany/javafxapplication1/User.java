ca964d52b8a4d5cc4248bc46bcf5a31c8ec0288b
Pikabomb1 <135108254+Pikabomb1@users.noreply.github.com>
2026-02-15 16:11:57 +0000

    Update User.java

--- -COMP20081-Systems-Software-202526-main/-COMP20081-Systems-Software-202526-main/cwk/JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/User.java
+++ -COMP20081-Systems-Software-202526-main/-COMP20081-Systems-Software-202526-main/cwk/JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/User.java
@@ -1,7 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+ 
+ 
+ 
 package com.mycompany.javafxapplication1;
 
 import javafx.beans.property.SimpleStringProperty;
